571209713e2199d2bbcc34f849822ce251d2a870
Dominic Bosch <dominic.bosch.db@gmail.com>
2015-06-03 11:08:12 +0200

    commented out server part, we jsut want data

--- index.js
+++ index.js
@@ -8,43 +8,45 @@ console.log( 'All arguments found, loading libraries' );
 var app, server, evt,
 	i = 1,
 	fs = require( 'fs' ),
-	util = require( 'util' ),
-	express = require( 'express' ),
-	socketio = require( 'socket.io' ),
+	// express = require( 'express' ),
+	// socketio = require( 'socket.io' ),
 	fileName = process.argv[ 2 ],
 	modTest = require( __dirname + '/modules/' + fileName ),
 	interval = parseInt( process.argv[ 3 ] ) * 1000;
 
-console.log( 'Libraries loaded, Starting server' );
+// console.log( 'Libraries loaded, Starting server' );
 
-app = express();
-app.use( express.static( 'webpage' ) );
-server = app.listen( 8642, function() {
-	console.log( 'server listening on port ' + 8642 );
-});
-io = socketio.listen( server );
-io.on( 'connection', function (socket) {
-	console.log( 'socketio has new connection...' );
-	fs.readFile( 'logs/' + fileName + '.log', 'utf8', function (err, data) {
-		if( !err ) {
-			var arr = data.split( '\n' );
-			io.emit( 'init', { num: arr.length - 1 } );
-			for( var i = 0; i < arr.length - 1; i++ ) {
-				io.emit( 'memusage', JSON.parse( arr[ i ] ) );
-			}
-			io.emit( 'endinit' );
-		}
-		data = null;
-		arr = null;
-		err = null;
-	});
-	socket.on( 'disconnect', function() {
-		console.log( 'user disconnected' );
-	});
-	socket = null;
-});
+// app = express();
+// app.use( express.static( 'webpage' ) );
+// server = app.listen( 8642, function() {
+// 	console.log( 'server listening on port ' + 8642 );
+// });
+// io = socketio.listen( server );
+// io.on( 'connection', function (socket) {
+// 	console.log( 'socketio has new connection...' );
+// 	fs.readFile( 'logs/' + fileName + '.log', 'utf8', function (err, data) {
+// 		if( !err ) {
+// 			var arr = data.split( '\n' );
+// 			io.emit( 'init', { num: arr.length - 1 } );
+// 			for( var i = 0; i < arr.length - 1; i++ ) {
+// 				io.emit( 'memusage', JSON.parse( arr[ i ] ) );
+// 			}
+// 			io.emit( 'endinit' );
+// 		}
+// 		data = null;
+// 		arr = null;
+// 		err = null;
+// 	});
+// 	socket.on( 'disconnect', function() {
+// 		console.log( 'user disconnected' );
+// 	});
+// 	socket = null;
+// });
 
-console.log( 'Server started, Starting memoy leaking script' );
+// console.log( 'Server started, Starting memoy leaking script' );
+
+console.log( 'Starting memoy leaking script' );
+fs.writeFileSync( 'logs/' + fileName + '.log', '' );
 
 function doLoop() {
 	console.log( 'Looping: ' + i++ );
@@ -52,7 +54,7 @@ function doLoop() {
 	evt.category = 'before';
 	evt.timestamp = (new Date()).getTime();
 	fs.appendFileSync( 'logs/' + fileName + '.log', JSON.stringify( evt ) + '\n' );
-	io.emit( 'memusage', evt );
+	// io.emit( 'memusage', evt );
 	
 	modTest.test();
 
@@ -60,7 +62,7 @@ function doLoop() {
 	evt.category = 'after';
 	evt.timestamp = (new Date()).getTime();
 	fs.appendFileSync( 'logs/' + fileName + '.log', JSON.stringify( evt ) + '\n' );
-	io.emit( 'memusage', evt );
+	// io.emit( 'memusage', evt );
 
 	setTimeout( doLoop, interval );
 }
